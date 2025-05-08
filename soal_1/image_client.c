03c41c36619a2c913e7671c1c02c9b8aecfd23a3
Ahmad Fawwaz <92526178+fawwazjs@users.noreply.github.com>
2025-05-08 15:07:10 +0700

    Update image_client.c

--- soal_1/image_client.c
+++ soal_1/image_client.c
@@ -1,123 +1,96 @@
-// image_client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <time.h>
+#include <sys/socket.h>
 
 #define PORT 8080
-#define SERVER_IP "127.0.0.1"
-#define BUFFER_SIZE 5242880 // 5MB max buffer
+#define BUFFER_SIZE 4096
 
-void decrypt_file() {
+void send_decrypt_command(int sock) {
     char filename[256];
     printf("Enter the file name: ");
-    scanf("%s", filename);
+    scanf("%255s", filename);
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("Socket");
-        return;
-    }
+    // Prepare command
+    char command[BUFFER_SIZE];
+    snprintf(command, sizeof(command), "DECRYPT %s", filename);
 
-    struct sockaddr_in serv_addr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(PORT)
-    };
-    inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr);
+    // Send command to server
+    send(sock, command, strlen(command), 0);
 
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connect");
-        close(sock);
-        return;
-    }
-
-    char message[300];
-    snprintf(message, sizeof(message), "DECRYPT %s", filename);
-    send(sock, message, strlen(message), 0);
-
-    char response[256] = {0};
+    // Receive response from server
+    char response[BUFFER_SIZE] = {0};
     read(sock, response, sizeof(response));
-    printf("Server: %s\n", response);
-
-    close(sock);
+    printf("Server: Text decrypted and saved as %s\n", response);
 }
 
-void download_file() {
+void send_download_command(int sock) {
     char filename[256];
-    printf("Enter the file name: ");
-    scanf("%s", filename);
+    printf("Enter the JPEG filename to download: ");
+    scanf("%255s", filename);
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("Socket");
+    // Prepare command
+    char command[BUFFER_SIZE];
+    snprintf(command, sizeof(command), "DOWNLOAD %s", filename);
+
+    // Send command to server
+    send(sock, command, strlen(command), 0);
+
+    // Receive file data from server
+    FILE *file = fopen(filename, "wb");
+    if (!file) {
+        printf("Failed to create file %s\n", filename);
         return;
     }
 
-    struct sockaddr_in serv_addr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(PORT)
-    };
-    inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr);
-
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connect");
-        close(sock);
-        return;
+    int bytes_received;
+    char buffer[BUFFER_SIZE];
+    while ((bytes_received = read(sock, buffer, sizeof(buffer))) > 0) {
+        fwrite(buffer, 1, bytes_received, file);
+        if (bytes_received < BUFFER_SIZE) break;
     }
 
-    char message[300];
-    snprintf(message, sizeof(message), "DOWNLOAD %s", filename);
-    send(sock, message, strlen(message), 0);
-
-    char *buffer = malloc(BUFFER_SIZE);
-    if (!buffer) {
-        perror("Malloc");
-        close(sock);
-        return;
-    }
-
-    int bytes = read(sock, buffer, BUFFER_SIZE);
-    if (bytes > 0 && strncmp(buffer, "ERROR", 5) != 0) {
-        FILE *fp = fopen(filename, "wb");
-        if (fp) {
-            fwrite(buffer, 1, bytes, fp);
-            fclose(fp);
-            printf("Success! Image saved as %s\n", filename);
-        } else {
-            printf("ERROR: Cannot save image to disk\n");
-        }
-    } else {
-        printf("%s\n", buffer);
-    }
-
-    free(buffer);
-    close(sock);
+    fclose(file);
+    printf("File %s downloaded successfully.\n", filename);
 }
 
 int main() {
-    int choice;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
 
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Failed to connect to server.\n");
+        return 1;
+    }
+
+    int choice;
     while (1) {
         printf("\n| Image Decoder Client |\n");
         printf("1. Send input file to server\n");
         printf("2. Download file from server\n");
-        printf("3. Exit\n>> ");
+        printf("3. Exit\n");
+        printf(">> ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                decrypt_file();
+                send_decrypt_command(sock);
                 break;
             case 2:
-                download_file();
+                send_download_command(sock);
                 break;
             case 3:
-                printf("Exiting client...\n");
+                close(sock);
+                printf("Client exiting...\n");
                 return 0;
             default:
-                printf("Invalid choice. Try again.\n");
+                printf("Invalid choice, please try again.\n");
         }
     }
 
